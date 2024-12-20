f728b0f54754a13a4ca312b9abfab9f3e4dd5158
Davi Hasson Castro <davihasson@gmail.com>
2024-12-20 00:37:00 -0300

    chore: init project

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Secret Santa",
+  description: "A secret santa app for your family and friends",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased dark`}>{children}</body>
+    </html>
+  );
+}
